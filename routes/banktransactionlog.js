b4baef97debeb10cfaa55f55a5f6b8c3c8bcc43e
marcus1688 <148477064+marcus1688@users.noreply.github.com>
2025-12-11 17:54:56 +0800

    23

--- routes/banktransactionlog.js
+++ routes/banktransactionlog.js
@@ -62,28 +62,34 @@ router.get(
         status: { $in: ["approved", "rejected", "reverted"] },
         ...dateFilter,
       };
-      const [deposits, withdraws, bonuses, usercashout] = await Promise.all([
-        Deposit.find(queryCondition)
-          .select(
-            "username fullname bankname ownername transfernumber method transactionType amount status remark imageUrl createdAt processBy processtime _id reverted duplicateIP depositname"
-          )
-          .lean(),
-        Withdraw.find(queryCondition)
-          .select(
-            "username fullname bankname ownername transfernumber method transactionType amount status remark imageUrl createdAt processBy processtime _id reverted duplicateIP depositname"
-          )
-          .lean(),
-        Bonus.find(queryCondition)
-          .select(
-            "username fullname promotionnameEN method transactionType amount status remark createdAt processBy processtime _id reverted duplicateIP"
-          )
-          .lean(),
-        UserWalletCashOut.find(queryCondition)
-          .select(
-            "username fullname transactionId walletType transactionType amount status remark method processBy reverted revertedProcessBy createdAt _id duplicateIP"
-          )
-          .lean(),
-      ]);
+      const [deposits, withdraws, bonuses, usercashout, usercashin] =
+        await Promise.all([
+          Deposit.find(queryCondition)
+            .select(
+              "username fullname bankname ownername transactionId transfernumber method transactionType amount status remark imageUrl createdAt processBy processtime _id reverted duplicateIP depositname"
+            )
+            .lean(),
+          Withdraw.find(queryCondition)
+            .select(
+              "username fullname bankname ownername transactionId transfernumber method transactionType amount status remark imageUrl createdAt processBy processtime _id reverted duplicateIP depositname"
+            )
+            .lean(),
+          Bonus.find(queryCondition)
+            .select(
+              "username fullname promotionnameEN method transactionId transactionType amount status remark createdAt processBy processtime _id reverted duplicateIP"
+            )
+            .lean(),
+          UserWalletCashOut.find(queryCondition)
+            .select(
+              "username fullname transactionId walletType transactionType amount status remark method processBy reverted revertedProcessBy createdAt _id duplicateIP"
+            )
+            .lean(),
+          UserWalletCashIn.find(queryCondition)
+            .select(
+              "username fullname transactionId walletType transactionType amount status remark method processBy reverted revertedProcessBy createdAt _id duplicateIP"
+            )
+            .lean(),
+        ]);
       const formatTransaction = (transaction, type) => {
         const commonFields = {
           _id: transaction._id,
@@ -99,13 +105,14 @@ router.get(
           processtime: transaction.processtime,
           reverted: transaction.reverted,
           duplicateIP: transaction.duplicateIP,
+          transactionId: transaction.transactionId,
         };
         if (type === "bonus") {
           return {
             ...commonFields,
             promotionnameEN: transaction.promotionnameEN,
           };
-        } else if (type === "walletCashout") {
+        } else if (type === "walletCashout" || type === "walletCashin") {
           return {
             ...commonFields,
             walletType: transaction.walletType,
@@ -126,7 +133,8 @@ router.get(
         ...deposits.map((d) => formatTransaction(d, "deposit")),
         ...withdraws.map((w) => formatTransaction(w, "withdraw")),
         ...bonuses.map((b) => formatTransaction(b, "bonus")),
-        ...usercashout.map((b) => formatTransaction(b, "bonus")),
+        ...usercashout.map((c) => formatTransaction(c, "walletCashout")),
+        ...usercashin.map((c) => formatTransaction(c, "walletCashin")),
       ].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
       res.status(200).json({
         success: true,
